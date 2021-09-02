924e896fa279dac1c6c59c2349e3bbe81b58efbf
Ncesecrede <francescopm95@gmail.com>
2021-09-02 15:55:08 +0100

    Comics

---
+++ laravel-comics/resources/views/components/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+</head>
+<body>
+    <header>
+        @include('components.header')
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer>
+        @include('components.footer')
+    </footer>
+</body>
+</html>
---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('components.base')
+@section('title', 'Homepage')
+@section('content')
+    ciao 
+@endsection
