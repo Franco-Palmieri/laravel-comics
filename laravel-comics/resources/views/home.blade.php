f0bbb88966a04ee53bb9b35e1f215a7e2effaeca
Ncesecrede <francescopm95@gmail.com>
2021-09-04 13:06:55 +0100

    Comics

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -23,7 +23,40 @@
         </div>
     </div>
     <div class="main-bar">
-        <div class="container"></div>
+        <div class="container">
+            <div class="row content-main-bar">
+                <div class="icon-main-box">
+                    <div class="icon-main">
+                        <img src="/images/buy-comics-digital-comics.png" alt="">
+                    </div>
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div class="icon-main-box">
+                    <div class="icon-main">
+                        <img src="/images/buy-comics-merchandise.png" alt="">
+                    </div>
+                    <span>DC MERCHANDISE</span>
+                </div>
+                <div class="icon-main-box">
+                    <div class="icon-main">
+                        <img src="/images/buy-comics-subscriptions.png" alt="">
+                    </div>
+                    <span>SUBSCRIPTIONS</span>
+                </div>
+                <div class="icon-main-box">
+                    <div class="icon-main">
+                        <img src="/images/buy-comics-shop-locator.png" alt="">
+                    </div>
+                    <span>SHOP LOCATOR</span>
+                </div>
+                <div class="icon-main-box">
+                    <div class="icon-main">
+                        <img src="/images/buy-dc-power-visa.svg" alt="">
+                    </div>
+                    <span>DC POWERVISA</span>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 @endsection
